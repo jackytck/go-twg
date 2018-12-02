752536c4feb90444a4c2c07b8935d8147c1a52dc
Jacky Tang <jackytck@gmail.com>
2018-12-02 13:07:46 +0800

    :white_check_mark: test json handler

--- signal/signal_test.go
+++ signal/signal_test.go
@@ -1,11 +1,50 @@
 package signal
 
-import "testing"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestHandler(t *testing.T) {
-	t.Log("Nat 1")
-	// t.FailNow()
-	t.Log("Nat 2")
-	t.Error("Failed")
-	t.Error("Failed2")
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest(http.MethodGet, "", nil)
+	if err != nil {
+		t.Fatalf("http.NewRequest() err = %s", err)
+	}
+	Handler(w, r)
+
+	resp := w.Result()
+	if resp.StatusCode != 200 {
+		t.Fatalf("Handler() status = %d; want %d", resp.StatusCode, 200)
+	}
+
+	contentType := "Content-Type"
+	if resp.Header.Get(contentType) != "application/json" {
+		t.Errorf("Handler() Content-Type = %q; want %q", contentType, "application/json")
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("ioutil.ReadAll(resp.Body) err = %s", err)
+	}
+	var p Person
+	err = json.Unmarshal(data, &p)
+	if err != nil {
+		t.Fatalf("json.Unmarshal(res.Body) err = %s", err)
+	}
+	wantAge := 30
+	if p.Age != wantAge {
+		t.Errorf("person.Age = %d; want %d", p.Age, wantAge)
+	}
+	wantName := "Nat Nat"
+	if p.Name != wantName {
+		t.Errorf("person.Name = %s; want %s", p.Name, wantName)
+	}
+	wantOccupation := "Nurse"
+	if p.Occupation != wantOccupation {
+		t.Errorf("person.Occupation = %s; want %s", p.Occupation, wantOccupation)
+	}
 }

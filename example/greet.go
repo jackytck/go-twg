8b95ecee0a448ff444f1402e483708890190aacb
Jacky Tang <jackytck@gmail.com>
2018-12-06 23:16:27 +0800

    :memo: examples in godocs

--- example/greet.go
+++ example/greet.go
@@ -1,6 +1,13 @@
+// Package example is a package for demostrating examples in source code.
 package example
 
 import "fmt"
+
+type Demo struct{}
+
+func (d *Demo) Hello() {
+	fmt.Println("Hello")
+}
 
 // Hello prints out hello to the person provided.
 func Hello(name string) (string, error) {
--- example/greet_test.go
+++ example/greet_test.go
@@ -2,6 +2,41 @@ package example
 
 import "fmt"
 
+func Example() {
+	fmt.Println("Package level")
+
+	// Output:
+	// Package level
+}
+
+func Example_second() {
+	fmt.Println("Package level second")
+
+	// Output:
+	// Package level second
+}
+
+func ExampleDemo() {
+	fmt.Println("Struct level")
+
+	// Output:
+	// Struct level
+}
+
+func ExampleDemo_Hello() {
+	fmt.Println("Method level")
+
+	// Output:
+	// Method level
+}
+
+func ExampleDemo_Hello_second() {
+	fmt.Println("Method level second")
+
+	// Output:
+	// Method level second
+}
+
 func ExampleHello() {
 	greeting, err := Hello("Nat")
 	if err != nil {

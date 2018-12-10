5bfa4525bf26620cf7b1673c7dcb3ffde76b7348
Jacky Tang <jackytck@gmail.com>
2018-12-10 23:55:42 +0800

    :white_check_mark: pattern of table driven tests

---
+++ underscore/underscore_test.go
@@ -0,0 +1,21 @@
+package underscore
+
+import "testing"
+
+func TestCamel(t *testing.T) {
+	testCases := []struct {
+		arg  string
+		want string
+	}{
+		{"thisIsACamelCaseString", "this_is_a_camel_case_string"},
+		{"with a space", "with a space"},
+		{"endsWithA", "ends_with_a"},
+	}
+	for _, tc := range testCases {
+		got := Camel(tc.arg)
+		if got != tc.want {
+			t.Errorf("Camel(%q) = %q; want %q\n", tc.arg, got, tc.want)
+		}
+
+	}
+}

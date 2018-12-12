43fde81671be1c7a6f2a8d4651b2f261fae2b7ed
Jacky Tang <jackytck@gmail.com>
2018-12-12 22:09:19 +0800

    :white_check_mark: shared setup and teardown

---
+++ psql/users_test.go
@@ -0,0 +1,103 @@
+package psql
+
+import (
+	"database/sql"
+	"fmt"
+	"reflect"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+func TestUserStore(t *testing.T) {
+	const (
+		dropDB          = `DROP DATABASE IF EXISTS test_user_store;`
+		createDB        = `CREATE DATABASE test_user_store;`
+		createUserTable = `CREATE TABLE users (
+												 id SERIAL PRIMARY KEY,
+												 name TEXT,
+												 email TEXT UNIQUE NOT NULL
+											 );`
+	)
+
+	psql, err := sql.Open("postgres", "host=localhost port=5432 user=jacky sslmode=disable")
+	if err != nil {
+		panic(fmt.Errorf("sql.Open() err = %s", err))
+	}
+	defer psql.Close()
+
+	_, err = psql.Exec(dropDB)
+	if err != nil {
+		panic(fmt.Errorf("psql.Exec() err = %s", err))
+	}
+	_, err = psql.Exec(createDB)
+	if err != nil {
+		panic(fmt.Errorf("psql.Exec() err = %s", err))
+	}
+	// teardown
+	defer func() {
+		_, err = psql.Exec(dropDB)
+		if err != nil {
+			panic(fmt.Errorf("psql.Exec() err = %s", err))
+		}
+	}()
+
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=jacky sslmode=disable dbname=test_user_store")
+	if err != nil {
+		panic(fmt.Errorf("sql.Open() err = %s", err))
+	}
+	defer db.Close()
+	_, err = db.Exec(createUserTable)
+	if err != nil {
+		panic(fmt.Errorf("db.Exec() err = %s", err))
+	}
+
+	us := &UserStore{
+		sql: db,
+	}
+	t.Run("Find", testUserStore_Find(us))
+	t.Run("Create", testUserStore_Find(us))
+	t.Run("Delete", testUserStore_Find(us))
+	t.Run("Subscribe", testUserStore_Find(us))
+	// teardown
+}
+
+func testUserStore_Find(us *UserStore) func(t *testing.T) {
+	return func(t *testing.T) {
+		nat := &User{
+			Name:  "Nat Nat",
+			Email: "nat@nat.com",
+		}
+		err := us.Create(nat)
+		if err != nil {
+			t.Errorf("us.Create() err = %s", err)
+		}
+		defer func() {
+			err := us.Delete(nat.ID)
+			if err != nil {
+				t.Errorf("us.Delete() err = %s", err)
+			}
+		}()
+
+		tests := []struct {
+			name    string
+			id      int
+			want    *User
+			wantErr error
+		}{
+			{"Found", nat.ID, nat, nil},
+			{"Not Found", -1, nil, ErrNotFound},
+		}
+		for _, tc := range tests {
+			t.Run(tc.name, func(t *testing.T) {
+				got, err := us.Find(tc.id)
+				if err != tc.wantErr {
+					t.Errorf("us.Find() err = %s", err)
+				}
+				if !reflect.DeepEqual(got, tc.want) {
+					t.Errorf("us.Find() = %+v, want %+v", got, tc.want)
+				}
+			})
+		}
+	}
+}

858292c27d3b79031f2483afd760e884a417ebb1
Jacky Tang <jackytck@gmail.com>
2019-01-31 23:42:57 +0800

    :construction: more ways of dependency injections

--- logger/demo.go
+++ logger/demo.go
@@ -23,6 +23,49 @@ func DemoV2(logger *log.Logger) {
 	}
 }
 
+// DemoV3 injects as a function.
+func DemoV3(logFn func(...interface{})) {
+	err := doTheThing()
+	if err != nil {
+		logFn("error in doTheThing():", err)
+	}
+}
+
+// Logger represents the dependent interface.
+type Logger interface {
+	Println(...interface{})
+	Printf(string, ...interface{})
+}
+
+// DemoV4 injects with interface.
+// logger := log.New(...)
+// DemoV4(logger)
+func DemoV4(logger Logger) {
+	err := doTheThing()
+	if err != nil {
+		logger.Println("error in doTheThing():", err)
+		logger.Printf("error in doTheThing(): %v\n", err)
+	}
+}
+
+// Thing injects as a struct.
+// Then no need to pass around the Logger.
+type Thing struct {
+	Logger interface {
+		Println(...interface{})
+		Printf(string, ...interface{})
+	}
+}
+
+// DemoV5 uses struct as dependency.
+func (t Thing) DemoV5() {
+	err := doTheThing()
+	if err != nil {
+		t.Logger.Println("error in doTheThing():", err)
+		t.Logger.Printf("error in doTheThing(): %v\n", err)
+	}
+}
+
 func doTheThing() error {
 	return errors.New("error opening file: nat.txt")
 }

7aae1468fe57490cb4fdaa8ecafeb8b92acf570a
Jacky Tang <jackytck@gmail.com>
2019-02-03 10:38:08 +0800

    :white_check_mark: tests for dependency injections

---
+++ logger/demo_test.go
@@ -0,0 +1,71 @@
+package logger_test
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"testing"
+
+	"github.com/jackytck/twg/logger"
+)
+
+type fakeLogger struct {
+	sb strings.Builder
+}
+
+func (f *fakeLogger) Println(v ...interface{}) {
+	fmt.Fprintln(&f.sb, v...)
+}
+
+func (f *fakeLogger) Printf(format string, v ...interface{}) {
+	fmt.Fprintf(&f.sb, format, v...)
+}
+
+func TestDemoV3(t *testing.T) {
+	var fl fakeLogger
+	logger.DemoV3(fl.Println)
+	want := "error in doTheThing():"
+	got := fl.sb.String()
+	if !strings.Contains(got, want) {
+		t.Errorf("Logs = %q; want substring %q", got, want)
+	}
+}
+
+func TestDemoV4(t *testing.T) {
+	var fl fakeLogger
+	logger.DemoV4(&fl)
+	want := "error in doTheThing():"
+	got := fl.sb.String()
+	if !strings.Contains(got, want) {
+		t.Errorf("Logs = %q; want substring %q", got, want)
+	}
+}
+
+func TestDemoV5(t *testing.T) {
+	var sb strings.Builder
+	testLogger := log.New(&sb, "", 0)
+	thing := logger.Thing{
+		Logger: testLogger,
+	}
+	thing.DemoV5()
+
+	want := "error in doTheThing():"
+	got := sb.String()
+	if !strings.Contains(got, want) {
+		t.Errorf("Logs = %q; want substring %q", got, want)
+	}
+}
+
+func TestDemoV5b(t *testing.T) {
+	var testLogger fakeLogger
+	thing := logger.Thing{
+		Logger: &testLogger,
+	}
+	thing.DemoV5()
+
+	want := "error opening file: nat.txt"
+	got := testLogger.sb.String()
+	if !strings.Contains(got, want) {
+		t.Errorf("Logs = %q; want substring %q", got, want)
+	}
+}
